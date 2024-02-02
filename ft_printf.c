094d9ba4ac90c9125fe8ac16aa18e6045ef3aecb
Artur Harutyunyan <arturhar@c2r6s2.42yerevan.am>
2024-02-02 13:43:30 +0400

    Norminette, bug fixes

--- ft_printf.c
+++ ft_printf.c
@@ -1,65 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arturhar <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/02 13:33:20 by arturhar          #+#    #+#             */
+/*   Updated: 2024/02/02 13:35:46 by arturhar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 #include "ft_printf.h"
 
-int ft_printf(const char *str, ...)
+int	ft_printf(const char *str, ...)
 {
-        int i;
-        int length;
-        va_list args;
-        
-        i = 0;
-        length = 0;
-        va_start(args, str);
-        while (str[i])
-        {
-                if (str[i] == '%')
-                {
-                        i++;
-                        length += format_parser(args, str[i]);
-                }
-                else
-                {
-                        length += ft_putchar(str[i]);
-                }
-                i++;
-        }
-        return (length);
-}
+	int		i;
+	int		length;
+	va_list	args;
 
-//int main() {
-//    ft_printf("ft_printf\nSingle character - %c"
-//              "\nString - %s\n"
-//              "Void * as hexidecimal - %p\n"
-//              "Decimal in base 10 - %d\n"
-//              "Integer in base10 - %i\n"
-//              "Unsigned decimal base 10 - %u\n"
-//              "Number in hexidecimal uppercase - %X\n"
-//              "Number in hexidecimal lowercase - %x\n"
-//              "Percent sign - %%\n",
-//              'a',
-//              "String of characters!",
-//              (void *) 123456798,
-//              -42,
-//              42,
-//              42,
-//              123456789,
-//              123456789
-//    );
-//    printf("Original\nSingle character - %c"
-//              "\nString - %s\n"
-//              "Void * as hexidecimal - %p\n"
-//              "Decimal in base 10 - %d\n"
-//              "Integer in base10 - %i\n"
-//              "Unsigned decimal base 10 - %u\n"
-//              "Number in hexidecimal uppercase - %X\n"
-//              "Number in hexidecimal lowercase - %x\n"
-//              "Percent sign - %%\n",
-//              'a',
-//              "String of characters!",
-//              (void *)123456798,
-//              -42,
-//              42,
-//              42,
-//              123456789,
-//              123456789
-//    );
-//}
+	i = 0;
+	length = 0;
+	va_start(args, str);
+	while (str[i])
+	{
+		if (str[i] == '%')
+		{
+			i++;
+			if (str[i] == '%')
+				length += ft_putchar('%');
+			length += format_parser(args, str[i]);
+		}
+		else
+		{
+			length += ft_putchar(str[i]);
+		}
+		i++;
+	}
+	return (length);
+}
